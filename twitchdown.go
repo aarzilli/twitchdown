7709eba804ff9d809e226fcc3ca9801e12aa126b
aarzilli <alessandro.arzilli@gmail.com>
2015-03-04 09:17:04 +0100

    Recover interrupted downloads

--- twitchdown.go
+++ twitchdown.go
@@ -16,7 +16,10 @@ import (
 	"strings"
 )
 
+const DEBUG = false
+
 var TwitchUrl = regexp.MustCompile(`^https?://(?:www\.)?twitch\.tv/[^/]+/v/(\d+)`)
+var PartUrl = regexp.MustCompile(`\?start_offset=(\d+)&end_offset=(\d+)$`)
 
 func must(err error, msg string) {
 	if err != nil {
@@ -109,17 +112,10 @@ func parseVideoId(arg string) int {
 	return -1
 }
 
-func setupOutput(videoId int, name string) io.WriteCloser {
-	if name != "" {
-		fh, err := os.Create(fmt.Sprintf("%s.ts", name))
-		must(err, "Could not create output file")
-		return fh
-	} else {
-		fh, err := os.Create(fmt.Sprintf("%d.ts", videoId))
-		must(err, "Could not create output file")
-		return fh
-	}
-
+func setupOutput(fileName string) io.WriteCloser {
+	fh, err := os.Create(fileName)
+	must(err, "Could not create output file")
+	return fh
 }
 
 func downloadStream(playlist m3u.Playlist, w io.Writer, startPosition int, endPosition int) {
@@ -130,21 +126,69 @@ func downloadStream(playlist m3u.Playlist, w io.Writer, startPosition int, endPo
 	for i := startPosition; i <= end; i++ {
 		fmt.Printf("\rDownloading part %d of %d and stopping at %d..", i, len(playlist), end)
 		resp, err := http.Get(playlist[i].Path)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error while downloading: %v\n", err)
-			return
-		}
+		must(err, "Error while downloading")
 		_, err = io.Copy(w, resp.Body)
 		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error while downloading: %v\n", err)
-			return
-		}
+		must(err, "Error while downloading")
 	}
 	fmt.Printf("\nDone\n")
 }
 
+func continueDownload(fileName string, playlist m3u.Playlist) (int, io.WriteCloser) {
+	fh, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return 0, setupOutput(fileName)
+		}
+		must(err, "Error opening output file")
+	}
+
+	fi, err := fh.Stat()
+	must(err, "Can not stat output file")
+	dldSz := fi.Size()
+	acc := int64(0)
+
+	for i := range playlist {
+		m := PartUrl.FindStringSubmatch(playlist[i].Path)
+		if m == nil || len(m) != 3 {
+			fmt.Printf("Failed to parse part url, can not continue download\n")
+			os.Exit(1)
+		}
+
+		startOffset, err := strconv.Atoi(m[1])
+		must(err, "Failed to parse part url, can not continue download")
+		endOffset, err := strconv.Atoi(m[2])
+		must(err, "Failed to parse part url, can not continue download")
+
+		partSz := int64((endOffset - startOffset) + 1)
+
+		if acc+partSz > dldSz {
+			toSkip := dldSz - acc
+			if DEBUG {
+				fmt.Printf("Download continues from part %d, skipping %d bytes\n", i, toSkip)
+			}
+
+			resp, err := http.Get(playlist[i].Path)
+			must(err, "Error while downloading")
+			defer resp.Body.Close()
+			bs, err := ioutil.ReadAll(resp.Body)
+			must(err, "Error while downloading")
+
+			_, err = fh.Write(bs[toSkip:])
+			must(err, "Error while downloading")
+
+			return i + 1, fh
+		}
+		acc += partSz
+	}
+
+	fmt.Fprintf(os.Stderr, "Nothing new to continue\n")
+	os.Exit(0)
+	return 0, nil
+}
+
 func main() {
+	continueDld := flag.Bool("c", false, "Continues interrupted download")
 	quality := flag.String("q", "high", "Selects video quality (defaults to 'high')")
 	position := flag.Int("p", 0, "Selects starting position (defaults to 0)")
 	end := flag.Int("e", -1, "Selects ending position (defaults to full vod)")
@@ -160,11 +204,31 @@ func main() {
 
 	videoId := parseVideoId(args[0])
 
+	fileName := ""
+	if *name != "" {
+		fileName = fmt.Sprintf("%s.ts", *name)
+	} else {
+		fileName = fmt.Sprintf("%d.ts", videoId)
+	}
+
 	sig, token := getAccessToken(videoId)
 	//fmt.Printf("sig=%s\ntoken=%s\n", sig, token)
 	playlist := getPlaylist(videoId, *quality, sig, token)
 
-	w := setupOutput(videoId, *name)
+	if DEBUG {
+		fmt.Printf("PARTS:\n")
+		for i := range playlist {
+			fmt.Printf("%03d %s\n", i, playlist[i].Path)
+		}
+		fmt.Printf("\n")
+	}
+
+	var w io.WriteCloser
+	if *continueDld && *position == 0 {
+		*position, w = continueDownload(fileName, playlist)
+	} else {
+		w = setupOutput(fileName)
+	}
 	defer w.Close()
 	downloadStream(playlist, w, *position, *end)
 }
